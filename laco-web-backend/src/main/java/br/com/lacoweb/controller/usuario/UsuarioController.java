80d1df7490825093c2b7b4805f8a791ac497bf22
Eduardo Felipe Righes <dudu_righespb@hotmail.com>
2025-04-30 15:34:56 -0300

    ajuste

--- laco-web-backend/src/main/java/br/com/lacoweb/controller/usuario/UsuarioController.java
+++ laco-web-backend/src/main/java/br/com/lacoweb/controller/usuario/UsuarioController.java
@@ -2,7 +2,7 @@ package br.com.lacoweb.controller.usuario;
 
 import br.com.lacoweb.domain.dto.usuario.UsuarioCadastroForm;
 import br.com.lacoweb.domain.service.usuario.UsuarioService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UsuarioController {
 
     private UsuarioService usuarioService;
